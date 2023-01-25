38a585e942e0718c5a3bdbf6e5d55de62979f9c2
OmverYo <justinlee990106@gmail.com>
2023-01-25 18:56:19 +1300

    Editting Card section

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,21 +1,20 @@
 import React from "react"
 import GraduationPicture from "./../Images/graduation-picture.jpg"
+import Star from "./../Images/red-star.png"
 
 export default function Card() {
     return (
         <div className="card">
-            <div className="card--image">
-                <img src={GraduationPicture}
-                    className="card--instance" alt="instance"></img>
-                    <p className="card--status">Sold</p>
+            <img src={GraduationPicture}
+                className="card--instance" alt="instance"></img>
+            <div className="card--status">
+                <img src={Star} className="card--star" alt="star"></img>
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">NZ</span>
             </div>
-            <p className="card--text">
-                5.0 (6) NZ
-                <br></br>
-                Graduation with Justin Lee
-                <br></br>
-                <b>From $50 </b>/ person
-            </p>
+            <p>Graduation with Justin Lee</p>
+            <p><span className="bold">From $50</span> / person</p>
         </div>
     )
 }
